f955f1a2b0c7e98131555d9d36469f0b0f862b92
AY <anthonyyim@gmail.com>
2013-04-14 14:35:06 -0400

    First commit of Main.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tacocat
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
